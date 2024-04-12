c2786765ae25d01236d441f9d919e5fd1e0f9adb
sujanyaD <soujanya.gh@gmail.com>
2024-04-12 18:18:17 -0400

    week 1 annalysis done

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 const PI = 3.1415
 const radius = 5;
 const area = PI * radius * radius;//total area
-const Plants = 20; // given intial plants
-const wk1 =1;
-const wk2 =2;
-const wk3 =3;
+let Plants = 20; // given intial plants
+
 const p1= 0.8; // given minimum space for each plant.
 
-//total area
-console.log(area);
+//total area plants can occupy
+const maxPlantArea = area / p1;
+console.log(` Total area of plants can occupy :${maxPlantArea}`)
 
-const area1 = Plants * p1; //area for 20 plants
+console.log(`Total Area :${area}`);//Total area
 
-console.log(area1);
+const area1 = Plants * p1; //area for 20 plants in week 1
 
-// switch (week1) {
-//     case 1: noOfPlants = noOfPlants *2;
-//         }
+console.log(`Area occupied by plants in week1 :${area1}`);
 
 const week = 1;
 //condition to calulate area for week 1
 if (week == 1)
 {
- noOfPlants=Plants * 2; // logic for plants doubled
- console.log(noOfPlants);
- let areaw1 = noOfPlants * p1; // toatl area after plants doubled 
- console.log(areaw1)
- const eiperArea = area * (80/100); // eighty percent of area caluculation.
- console.log(eiperArea);
-
- let canBePruned = areaw1 <= eiperArea;
- console.log(canBePruned);
+    // logic for plants doubled 
+ Plants=Plants * 2; 
+ console.log(`plants Doubled every week: ${Plants}`);
+// toatl area after plants doubled 40* 0.8
+ let areaw1 = Plants * p1; 
+  console.log(`Total area occupied after plants doubled: ${areaw1}`)
+ // eighty percent of area caluculation.
+ const eiperArea = area * (80/100);
+ console.log(`Area of plants :${eiperArea}`);
+// area of week 1 is greater than eighty percent ?
+ let canBePruned = areaw1 >= eiperArea; 
+ console.log(`condition for pruning  met : ${canBePruned}`); 
+//Area for fiftypercent plant capacity
+ const areaFiftyPer = area * (50/100); 
+// fifty percent of area caluclation.
+ const fiftyPerPlntCnt =areaFiftyPer / p1;
+ console.log(`Area caluculated for fiftypercent: ${areaFiftyPer}`)
+ // Eighty percent area caluculation
+ const eigtyPercentPlntCnt = eiperArea /p1;
+ console.log(`plant count for eighty percent area:${eigtyPercentPlntCnt}`)
+// can be monitored condition
+ const monitored =( fiftyPerPlntCnt>Plants)&&(Plants<eigtyPercentPlntCnt);
+ console.log(monitored)
+ // planted 
+ const planted =fiftyPerPlntCnt<Plants;
+ console.log(planted)
 }
+
 
 
  
